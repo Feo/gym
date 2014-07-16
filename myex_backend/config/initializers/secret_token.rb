9c82708f86b8f347a1b3df8bc77ac1152b8e81af
陈诚 <chencheng@chenchengs-MacBook-Pro.local>
2014-07-16 14:31:43 +0800

    Rails initialization.

---
+++ myex_backend/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MyexBackend::Application.config.secret_token = '0e7b9632b45f987ae9f86750dcf6452e95ffbed45d51983d6625d5a8e686b333f78cccaade2baf70027463b87765da1d1136c21212fa83429ab1eed392c130a8'
