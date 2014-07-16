995355d2c49c1dbc1fe3e5fb04847002de7738c5
陈诚 <chencheng@chenchengs-MacBook-Pro.local>
2014-07-16 17:44:26 +0800

    Add api login, logout, update and info for coaches.

--- myex_backend/lib/api/helpers.rb
+++ myex_backend/lib/api/helpers.rb
@@ -1,5 +1,24 @@
 module API
   module APIHelpers
-    
+    def current_coach
+      @current_coach ||= Coach.find_by_remember_token(cookies[:remember_token])
+    end
+
+    def authenticate!
+      error!('未登录。', 401) unless current_coach
+    end
+
+    def sign_in_coach(coach)
+      if params[:remember_me]
+        cookies.permanent[:remember_token] = coach.remember_token
+      else
+        cookies[:remember_token] = coach.remember_token
+      end
+      self.current_coach = coach
+    end
+
+    def current_coach=(coach)
+      @current_coach = coach
+    end
   end
 end
--- myex_backend/lib/api/v1/coaches.rb
+++ myex_backend/lib/api/v1/coaches.rb
@@ -11,11 +11,53 @@ module API
           if !Coach.find_by_email(params[:coach][:email]).nil?
             error!({"error" => "该邮箱已被使用，请重新输入。" }, 400)
           elsif @coach.save
+            sign_in_coach @coach
             present @coach
           else
             error!({"error" => "注册失败。" }, 400)
           end
         end
+
+        desc "Coach login"
+        post 'login' do
+          @coach = Coach.find_by_email(params[:session][:email].downcase)
+          if params[:session][:password] != params[:session][:password_confirmation]
+            error!({"error" => "邮箱或密码错误。" }, 400)
+          elsif @coach && @coach.authenticate(params[:session][:password])
+            sign_in_coach @coach
+            present @coach
+          else
+            error!({"error" => "邮箱或密码错误。" }, 400)
+          end
+        end
+
+        desc "Coach logout"
+        post 'logout' do
+          @coach = self.current_coach
+          self.current_coach = nil
+          cookies[:remember_token] = nil
+          present @coach
+        end
+
+        desc "Update coach information"
+        post 'update' do
+          @coach = current_coach
+          if params[:coach][:password] != params[:coach][:password_confirmation]
+            error!({"error" => "确认密码错误。" }, 400)
+          elsif @coach.update_attributes(params[:coach])
+            sign_in_coach @coach
+            present @coach
+          else
+            error!({"error" => "修改教练信息不成功。" }, 400)
+          end
+        end
+
+        desc "Get coach information"
+        get 'info' do
+          present current_coach
+        end
+
+
       end
     end
   end
