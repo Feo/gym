e10e1726b886acd4abca4a12b7005e6cf6299a46
陈诚 <chencheng@chenchengs-MacBook-Pro.local>
2014-08-05 11:46:52 +0800

    Add api create, update and delete for memberhabits.

---
+++ myex_backend/lib/api/v1/memberhabits.rb
@@ -0,0 +1,48 @@
+module API
+  module V1
+    class Memberhabits < Grape::API
+      version 'v1'
+      format :json
+
+      resource :memberhabits do
+        
+        desc "Create  member habit table."
+        post 'create' do
+          @habit = MemberHabit.new(params[:habit])
+          if !params[:habit][:member_id].empty? && !MemberHabit.find_by_member_id(params[:habit][:member_id]).nil?
+            error!({"error" => "该会员已创建生活健康习惯表。" }, 400)
+          elsif @habit.save
+            present @habit
+          else
+            error!({"error" => "创建生活健康习惯表失败。" }, 400)
+          end
+        end
+
+        desc "Update  member habit table."
+        post 'update' do
+          @habit = MemberHabit.find_by_member_id(params[:habit][:member_id])
+          if !@habit
+            error!({"error" => "改会员生活健康习惯表不存在。" }, 400)
+          elsif @habit.update_attributes(params[:habit])
+            present @habit
+          else
+            error!({"error" => "修改生活健康习惯表失败。" }, 400)
+          end
+        end
+
+        desc "Delete  member habit table."
+        post 'delete' do
+          @habit = MemberHabit.find_by_member_id(params[:habit][:member_id])
+          if !@habit
+            error!({"error" => "改会员生活健康习惯表不存在。" }, 400)
+          elsif @habit.delete
+            present @habit
+          else
+            error!({"error" => "修改生活健康习惯表失败。" }, 400)
+          end
+        end
+
+      end
+    end
+  end
+end
--- myex_backend/lib/api/v1/root.rb
+++ myex_backend/lib/api/v1/root.rb
@@ -3,6 +3,7 @@ module API
     class Root < Grape::API
       mount API::V1::Coaches
       mount API::V1::Members
+      mount API::V1::Memberhabits
     end
   end
 end
